9e569d3c35eda5b7e4316dde3504d1c1172ed6ed
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-04-30 18:07:23 +0100

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "class25" {
   ami = var.my_ami  # data.aws_ami.ubuntu.id
   instance_type = var.my_instance
- user_data = file("${path.module}/app1-intsall.sh")
+ user_data = file("${path.module}/app1-intstall.sh")
 #vpc_security_group_ids = [ aws_security_group_vpc-ssh_id, aws_security_group_vpc-web-id ]
   key_name =  "juicy"
 
