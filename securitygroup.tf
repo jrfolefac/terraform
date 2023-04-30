5053befe5c9ed1d10dd2300b3ddd727b9b3c85c6
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-04-30 14:55:20 +0100

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -3,16 +3,16 @@ resource "aws_security_group" "vpc-ssh" {
   description = "dev ssh"
   
 
-  ingress  {
-    description      = "allow port 22"
+  ingress {
+    description      = "Allow port 22"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-   }
+ }
 
   egress {
-   description      = "allow all outbound"
+   description      = "Allow all outbound"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -22,5 +22,42 @@ resource "aws_security_group" "vpc-ssh" {
 
   tags = {
     Name = "vpc-ssh"
+  }
+}
+
+
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web"
+  description = "dev ssh"
+  
+
+  ingress {
+    description      = "Allow port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+ 
+  
+  ingress {
+    description      = "Allow port 443"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+ 
+  egress {
+   description      = "Allow all outbound"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "vpc-web"
   }
 }
