512ae9730ac6a6ee28113d596936a3708854e958
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-04-30 11:45:15 +0100

    Create datasource.tf

---
+++ datasource.tf
@@ -0,0 +1,26 @@
+data sources
+
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230325"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+   filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
