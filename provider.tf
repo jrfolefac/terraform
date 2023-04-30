d3a5b07a3c253630b0f12fb91287e37db8bd5729
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-04-30 10:03:34 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
-  required_version = "~> 0.14"
-  required_provider {
-     aws = "hashicop/aws"
-     version = "~> 3.0"                  
+  required_version = "~> 1.0"
+  required_providers {
+     aws = {
+     source = "hashicorp/aws"
+     version = "~> 3.0"
+     }
     }
 } 
 
