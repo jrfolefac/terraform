2f7836aa2674cb96a5b4b395e6954c848d59c3a3
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-05-07 10:15:31 +0100

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,8 +2,7 @@
 resource "aws_instance" "class25" {
   ami = var.my_ami  # data.aws_ami.ubuntu.id
   instance_type = var.my_instance
- #user_data = file("${path.module}/app1-intstall.sh")
-  #security_groups = [aws_security_group.webtraffic.name]
+ #user_data = file("${path.module}/app1-intstall.sh" 
   key_name =  "juicy"
 
 
