320bce3a7edc049aa5f7b51ba452fdc826cc2b43
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-05-07 10:07:30 +0100

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "webtraffic" {
       content {
       from_port = port.value
       to_port   = port.value
-     protocol   = "-1"
+     protocol   = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
     }
    }
