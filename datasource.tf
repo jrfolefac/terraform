106fde7cc0fa497970ce912d1e4bef0037543f66
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-05-07 11:52:28 -0700

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20230328"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
