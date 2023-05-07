7fe8da4f7ca7352e2b98c5b8051bf99f97568998
jrfolefac <132182783+jrfolefac@users.noreply.github.com>
2023-05-07 10:58:01 +0100

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "class25" {
   ami = var.my_ami  # data.aws_ami.ubuntu.id
-  instance_type = var.my_instance
+  instance_type = var.my_instance["dev"]
  #user_data = file("${path.module}/app1-intstall.sh" 
   key_name =  "juicy"
   count = 2
